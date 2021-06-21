32face7f92c155a29f03b3084e950ec2367b727f
EspoirBao <51221361+EspoirBao@users.noreply.github.com>
2021-06-21 15:37:17 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,9 @@
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
+  <button type="button" @click="state.count++">
+    count is: {{ state.count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -17,13 +19,61 @@
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
-
+import {
+  defineProps,
+  reactive,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onErrorCaptured,
+  onRenderTriggered,
+} from "vue";
 defineProps({
-  msg: String
-})
+  // vue3Props
+  msg: String,
+});
 
-const state = reactive({ count: 0 })
+// vue3生命周期钩子
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+onMounted(() => {
+  console.log("onMounted");
+});
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate");
+});
+onUpdated(() => {
+  console.log("onUpdated");
+});
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount");
+});
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
+onActivated(() => {
+  console.log("onActivated");
+});
+onDeactivated(() => {
+  console.log("onDeactivated");
+});
+onErrorCaptured(() => {
+  console.log("onErrorCaptured");
+});
+
+onRenderTriggered((e) => {
+  debugger;
+  // 检查哪个依赖项导致组件重新呈现
+  console.log("onErrorCaptured");
+});
+
+const state = reactive({ count: 0 });
 </script>
 
 <style scoped>
